4aebfc7ac326a653fa55ced58aa558cb767dbce9
berketok-git <mail@berketok.com>
2024-04-27 12:11:46 +0300

    Update API endpoint in tools.js

--- tools.js
+++ tools.js
@@ -24,7 +24,7 @@ const PRODUCT_FINDER = {
   runCmd: async ({ product }) => {
     try {
       const { data } = await axios.get(
-        `https://api.npoint.io/2c308c84cfe292772cdf/items/${encodeURIComponent(product)}`
+        `https://api.npoint.io/2c308c84cfe292772cdf/items/item/${encodeURIComponent(product)}`
       );
       return JSON.stringify(data);
     } catch (err) {
